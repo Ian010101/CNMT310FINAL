265a81244c6bfdb8a527ade413b751e82b715ceb
Manny Estrada <mestr595@uwsp.edu>
2023-04-11 22:39:44 -0500

    Logs user out and redirects to HOME

--- logout.php
+++ logout.php
@@ -1,3 +1,17 @@
 <?php
 
-// Insert logout functionality and redirect to HOME 
+require_once('autoload.php');
+
+// set session variable to false and unset it
+$_SESSION["logged_in"] = false;
+unset($_SESSION["logged_in"]);
+
+// destroy the session and remove all session variables
+session_destroy();
+$_SESSION = array();
+
+// close session
+session_write_close();
+
+// redirect to home page
+die(header("Location: " . HOME));
