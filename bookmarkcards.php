87ea8a051e3654387a677cbdb5f96ad5f470187c
Manny Estrada <mestr595@uwsp.edu>
2023-04-21 19:36:20 -0500

    Creates bookmark cards

---
+++ bookmarkcards.php
@@ -0,0 +1,33 @@
+<?php
+
+// Add redirects to prevent direct access
+
+// Function to create bookmark cards
+function generateBookmarkCards($bookmarks) {
+    // Loop through bookmarks array and generate a card for each bookmark
+    foreach ($bookmarks as $bookmark) {
+        // Bookmark card 
+        print '<div class="d-flex justify-content-center align-items-center mb-2">';
+            print '<div class="container">';
+                print '<div class="row d-flex justify-content-center">';
+                    print '<div class="col-12 col-md-8 col-lg-10">';
+                        print '<div class="card bg-white">';
+                            print '<div class="card-body p-3">';
+                                print '<div>';
+                                    print '<div class="float-end">';
+                                        // Delete bookmark button 
+                                        print '<button type="button" class="btn btn-light"><img class="deleteIcon" src="images/deleteIcon.png">&nbsp;&nbsp;Delete</button>';
+                                    print '</div>';
+                                    // Bookmark title
+                                    print '<a href="' . $bookmark->url . '" class="bookmarkTitle"><h5>' . $bookmark->displayname . '</h5></a>';
+                                    // Bookmark url 
+                                    print '<a href="' . $bookmark->url . '" class="bookmarkURL"><span>' . $bookmark->url . '</span></a>';
+                                print '</div>';
+                            print '</div>';    
+                        print '</div>';
+                    print '</div>';    
+                print '</div>';
+            print '</div>';
+        print '</div>';
+    }
+}
--- bookmarks.php
+++ bookmarks.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("autoload.php");
+require_once("getbookmarks.php");
 require_once("classes/SitePage.class.php");
 
 // check if the user is not logged in
@@ -8,14 +9,14 @@ if (!isset($_SESSION['id'])) {
     die(header("Location: " . LOGIN));
 } 
 
-$page = new SitePage("Homepage");
+$page = new SitePage("Bookmarks");
 
 print $page->getTopSection();
 
     // Navigation bar 
     print '<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">';
         print '<div class="container-fluid mx-5 px-5 py-2">';
-            print '<a class="navbar-brand" href="' . HOME . '">Project Name</a>';
+            print '<a class="navbar-brand sparkBranding" href="' . HOME . '"><img src="images/logo.png" width="50" height="50" class="d-inline-block align-top mx-2">Spark</a>';
             print '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">';
                 print '<span class="navbar-toggler-icon"></span>';
             print '</button>';
@@ -41,9 +42,10 @@ print $page->getTopSection();
             print '<div class="row d-flex justify-content-center">';
                 print '<div class="col-12 col-md-8 col-lg-10">';
                     print '<form class="d-flex">';
+                        // Search bar input and button
                         print '<input class="form-control me-1 inputFocus" type="search" placeholder="Search your bookmarks" aria-label="Search">';
                         print '<button class="btn btn-dark px-4" type="submit">';
-                            print '<img src="searchIcon.png">';
+                            print '<img src="images/searchIcon.png">';
                         print '</button>';
                     print '</form>';
                 print '</div>';
@@ -52,34 +54,53 @@ print $page->getTopSection();
     print '</div>'; 
     
     // Add bookmark button 
-    print '<div class="d-flex justify-content-center align-items-center mt-5">';
+    print '<div class="d-flex justify-content-center align-items-center mt-5 mb-3">';
         print '<div class="container">';
             print '<div class="row d-flex justify-content-center">';
                 print '<div class="col-12 col-md-8 col-lg-10">';
-                    print '<button class="btn purpleButton" type="submit">+ Add Bookmark</button>';
+                    print '<button class="btn purpleButton" type="submit" data-bs-toggle="modal" data-bs-target="#createModal">+ Add Bookmark</button>';
                 print '</div>';
             print '</div>';
         print '</div>';
     print '</div>';
 
-    // Bookmark card 
-    print '<div class="d-flex justify-content-center align-items-center mt-3">';
-        print '<div class="container">';
-            print '<div class="row d-flex justify-content-center">';
-                print '<div class="col-12 col-md-8 col-lg-10">';
-                    print '<div class="card bg-white">';
-                        print '<div class="card-body p-2">';
-                            print '<div>';
-                                print '<h5 class="bookmarkTitle">Bookmark Title</h5>';
-                                print '<p>Bookmark URL</p>';
-                            print '</div>';
-                        print '</div>';    
-                    print '</div>';
-                print '</div>';    
+    // Add bookmark modal 
+    print '<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">';
+        print '<div class="modal-dialog modal-lg">';
+            print '<div class="modal-content">';
+                print '<div class="modal-body p-4">';
+                    print '<button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Cancel"></button>';
+                    // Add bookmark form - uses addbookmark.php
+                    print '<form class="mb-3 mt-md-2" action="addbookmark.php" method="post">';
+                        print '<div class="text-center">';
+                            print '<h3 class="fw-bold mb-3">Add Bookmark</h3>';
+                        print '</div>';
+                        print '<div class="mb-3">';
+                            // Bookmark title input field 
+                            print '<label for="title" class="form-label ">Title</label>';
+                            print '<input type="text" class="form-control inputFocus" id="title" name="title" placeholder="Bookmark title" required>';
+                        print '</div>';
+                        print '<div class="mb-3">';
+                            // Bookmark URL input field - Added some very basic form validation using type="url" (can consider adding more complicated validation methods that might provide a better experience)
+                            print '<label for="url" class="form-label ">Link</label>';
+							print '<input type="url" class="form-control inputFocus" id="url" name="url" placeholder="https://example.com" required>';
+                        print '</div>';
+                        print '<div class="float-end">';
+                            print '<button type="button" class="btn purpleOutlineBtn" data-bs-dismiss="modal">Cancel</button>';
+                            // Form submit button 
+                            print '<button type="submit" class="btn purpleButton">Add</button>';
+                        print '</div>';
+                    print '</form>';
+                print '</div>';
             print '</div>';
         print '</div>';
     print '</div>';
 
+	// Get user's bookmarks from API
+	$bookmarks = getUserBookmarks();
+
+	// Generate HTML for bookmark cards
+	generateBookmarkCards($bookmarks);
 
     // Footer 
     print '<footer class="footer mt-auto py-3 bg-dark fixed-bottom">';
