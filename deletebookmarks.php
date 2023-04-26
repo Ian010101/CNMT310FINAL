a60317e7e2c7fcd4187af05f8034ce9e57e41b69
mestr595 <123401854+mestr595@users.noreply.github.com>
2023-04-25 22:53:52 -0500

    updated to get bookmark id from bookmark cards

--- deletebookmarks.php
+++ deletebookmarks.php
@@ -4,19 +4,29 @@ require_once("autoload.php");
 require_once("WebServiceClient.php");
 
 // change name to reflect your user credit file/location
-require_once(__DIR__ . "/usr_creds.php");
+require_once(__DIR__ . "/../usr_creds.php");
 
-//function deleteBookmark() {
-// set API endpoint url
-//function deleteBookmark($bookmarkID) {
-$url = "https://cnmt310.classconvo.com/bookmarks/";
+if (isset($_GET["bookmarkID"])) {
+    $bookmarkID = $_GET["bookmarkID"];
+    deleteBookmark($bookmarkID);
+} else {
+    die("Error: Bookmark ID not specified.");
+}
 
-$client = new WebServiceClient($url);
+function deleteBookmark($bookmarkID) {
+    // set API endpoint url
+    $url = "https://cnmt310.classconvo.com/bookmarks/";
 
-	$userID = $_SESSION["id"];
-    $bookmarkID = $_SESSION["bookmarkID"];
+    $client = new WebServiceClient($url);
+	$client->setMethod("GET");
+
+	
+    $userID = $_SESSION["id"];
     // create data array to send to the API
-    $data = array("bookmark_id" => $bookmarkID, "user_id" => $userID);
+    $data = array(
+			"bookmark_id" => $bookmarkID, 
+			"user_id" => $userID
+			);
     
     // set API action and auth fields
     $action = "deletebookmark";
@@ -38,21 +48,15 @@ $client = new WebServiceClient($url);
         die(print("Error, no result property"));
     }
 
-     // Check if the bookmarks were retrieved successfully
-     if (property_exists($obj, "result") && $obj->result == "Success") {
-        // Extract bookmark data from object
-        $bookmarks = $obj->data;
-        //echo $bookmarkID;
-        // Return array of bookmarks
-       //return $bookmarks;
-       die(header("Location: " . BOOKMARKS));
+    // Check if the bookmark was deleted successfully
+    if (property_exists($obj, "result") && $obj->result == "Success") {
+        die(header("Location: " . BOOKMARKS));
     } else {
         // Handle error
         if (property_exists($obj, "data") && property_exists($obj->data, "message")) {
             die("Error: " . $obj->data->message);
         } else {
-            die("Error: Unable to retrieve bookmarks.");
+            die("Error: Unable to delete bookmark.");
         }
     }
-
-//}
+}
