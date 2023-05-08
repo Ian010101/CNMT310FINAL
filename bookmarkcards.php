d6bd38ae09d54274ab0ba86decf920b3f6f86e9b
Ian Anhalt <96670734+Ian010101@users.noreply.github.com>
2023-05-08 11:15:46 -0500

    add visit changes

--- bookmarkcards.php
+++ bookmarkcards.php
@@ -1,4 +1,10 @@
 <?php
+//require_once("addvisit.php");
+print '<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">';
+print '<link rel="stylesheet" href="/resources/demos/style.css">';
+print '<script src="https://code.jquery.com/jquery-3.6.0.js"></script>';
+print '<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>';
+
 // Redirect if accessed directly
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     die(header("Location: " . BOOKMARKS));
@@ -25,9 +31,9 @@ function generateBookmarkCards($bookmarks) {
                                         print '</button>';
                                     print '</div>';
                                     // Bookmark title
-									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
+									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
                                     // Bookmark url 
-                                    print '<a href="' . $bookmark->url . '" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
+                                    print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
                                 print '</div>';
                             print '</div>';    
                         print '</div>';
