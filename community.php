8a3cf4a855fdfe27e5d17fd1ef8ba7d4532b2bb1
mestr595 <123401854+mestr595@users.noreply.github.com>
2023-05-09 17:45:56 -0500

    Added search, autocomplete, bookmark id visibility

--- community.php
+++ community.php
@@ -43,25 +43,44 @@ print '<div class="d-flex flex-column min-vh-100">';
         print '</div>';
     print '</nav>';
 
+    // Get user's bookmark titles, then adds them into an array
+    $bookmarks = getPublicBookmarks();
+    $bookmarksTitle = array();
+    foreach($bookmarks as $bookmark) {
+        array_push($bookmarksTitle, $bookmark->displayname);
+    }
 
-    // Search bar
-    print '<div class="d-flex justify-content-center align-items-center my-5">'; 
-        print '<div class="container">';
-            print '<div class="row d-flex justify-content-center">';
-                print '<div class="col-12 col-md-8 col-lg-10">';
-                    print '<form class="d-flex">';
-                        // Search bar input and button
-                        print '<input class="form-control me-1 inputFocus" type="search" placeholder="Search your bookmarks" aria-label="Search">';
-                        print '<button class="btn btn-dark px-4" type="submit">';
-                            print '<img src="images/searchIcon.png">';
-                        print '</button>';
-                    print '</form>';
-                print '</div>';
-            print '</div>';
-        print '</div>';
-    print '</div>'; 
+	print '<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">';
+	print '<script src="https://code.jquery.com/jquery-3.6.0.js"></script>';
+	print '<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>';
+
+    // Javascript autocomplete function, takes the data from the array just made
+    print '<script type="text/Javascript">
+        $( function() {
+            let bookmarks = ' . json_encode($bookmarksTitle) . ';
+            $( "#searchBar" ).autocomplete({
+                source: bookmarks ,
+                minLength: 1,
+            });
+        });
+    </script>';
+
+		// Search bar
+	   print '<div class="d-flex justify-content-center align-items-center my-5">
+		 <div class="container">
+			 <div class="row d-flex justify-content-center">
+				 <div class="col-12 col-md-8 col-lg-10">
+					 <form class="d-flex" action="searchpublicbookmarks.php" method="post">';						// Search bar input and button
+						print '<input class="form-control me-1 inputFocus" type="search" placeholder="Search your bookmarks" aria-label="Search" name="search_term" id="searchBar">';
+						print '<button class="btn btn-dark px-4" type="submit" name="search">';
+							print '<img src="images/searchIcon.png">';
+						print '</button>';
+					print '</form>';
+				print '</div>';
+			print '</div>';
+		print '</div>';
+	print '</div>';
  
-	//Edit bookmark cards or public bookmarks
     //Display public posts here
 	if(isset($_POST['search'])) {
     // Get search term from form data
@@ -74,9 +93,9 @@ print '<div class="d-flex flex-column min-vh-100">';
         return strpos($bookmark->displayname, $search_term) !== false;
     });
 
-    // Display matching bookmarks using generateBookmarkCards function
+    // Display matching bookmarks using generatePublicBookmarkCards function
     if(count($matching_bookmarks) > 0) {
-        generateBookmarkCards($matching_bookmarks);
+        generatePublicBookmarkCards($matching_bookmarks);
     } else {
         print '<div class="d-flex justify-content-center align-items-center mt-2">';
             print '<div class="container">';
@@ -92,7 +111,7 @@ print '<div class="d-flex flex-column min-vh-100">';
     // Display all bookmarks
     $bookmarks = getPublicBookmarks();
     if(count($bookmarks) > 0) {
-        generateBookmarkCards($bookmarks);
+        generatePublicBookmarkCards($bookmarks);
     } else {
         print '<div class="d-flex justify-content-center align-items-center mt-2">';
             print '<div class="container">';
