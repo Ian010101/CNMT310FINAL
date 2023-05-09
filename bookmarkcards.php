8289cdce4dae80c8ddb059f517a32526736f5dd1
mestr595 <123401854+mestr595@users.noreply.github.com>
2023-05-08 21:24:49 -0500

    Added function for creating public bookmarks

--- bookmarkcards.php
+++ bookmarkcards.php
@@ -1,14 +1,10 @@
 <?php
-//require_once("addvisit.php");
-print '<link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">';
-print '<link rel="stylesheet" href="/resources/demos/style.css">';
-print '<script src="https://code.jquery.com/jquery-3.6.0.js"></script>';
-print '<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>';
 
 // Redirect if accessed directly
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     die(header("Location: " . BOOKMARKS));
 }
+
 // Function to create bookmark cards
 function generateBookmarkCards($bookmarks) {
 	
@@ -31,9 +27,9 @@ function generateBookmarkCards($bookmarks) {
                                         print '</button>';
                                     print '</div>';
                                     // Bookmark title
-									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
+									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
                                     // Bookmark url 
-                                    print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
+                                    print '<a href="' . $bookmark->url . '" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
                                 print '</div>';
                             print '</div>';    
                         print '</div>';
@@ -42,5 +38,31 @@ function generateBookmarkCards($bookmarks) {
             print '</div>';
         print '</div>';
     }
+}
+
+// Function to create public bookmark cards
+function generatePublicBookmarkCards($bookmarks) {
 	
+    // Loop through bookmarks array and generate a card for each bookmark
+    foreach ($bookmarks as $bookmark) {
+        // Bookmark card 
+        print '<div class="d-flex justify-content-center align-items-center mb-2">';
+            print '<div class="container">';
+                print '<div class="row d-flex justify-content-center">';
+                    print '<div class="col-12 col-md-8 col-lg-10">';
+                        print '<div class="card bg-white">';
+                            print '<div class="card-body p-3">';
+                                print '<div>';
+                                    // Bookmark title
+									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
+                                    // Bookmark url 
+                                    print '<a href="' . $bookmark->url . '" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
+                                print '</div>';
+                            print '</div>';    
+                        print '</div>';
+                    print '</div>';    
+                print '</div>';
+            print '</div>';
+        print '</div>';
+    }
 }
