52dfe93f5eb77da3f0b941b4a236d7e863110e16
mestr595 <123401854+mestr595@users.noreply.github.com>
2023-05-09 00:32:07 -0500

    Redirect if accessed directly

--- action-login.php
+++ action-login.php
@@ -3,29 +3,29 @@
 require_once("autoload.php");
 require_once("WebServiceClient.php");
 
-// change name to reflect your user credit file/location
+// Change name to reflect your user credit file/location
 require_once(__DIR__ . "/../usr_creds.php");
 
-// check if the HTTP_REFERER header is set
+// Check if the HTTP_REFERER header is set
 if (!isset($_SERVER['HTTP_REFERER'])) {
     // redirect the user to another page
     die(header('Location: ' . HOME));
 }
-// set API endpoint url
+// Set API endpoint url
 $url = "https://cnmt310.classconvo.com/bookmarks/";
 
 $client = new WebServiceClient($url);
 
-// check if the login form is submitted
+// Check if the login form is submitted
 if (isset($_POST["submit"])) {
 
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // create data array to send to the API
+    // Create data array to send to the API
     $data = array("username" => $username, "password" => $password);
     
-    // set API action and auth fields
+    // Set API action and auth fields
     $action = "authenticate";
     $fields = array(
         "apikey" => APIKEY,
@@ -36,25 +36,25 @@ if (isset($_POST["submit"])) {
 
     $client->setPostFields($fields);
 
-    // call API and get response
+    // Call API and get response
     $returnValue = $client->send();
 
-    // decode JSON response into php object
+    // Decode JSON response into php object
     $obj = json_decode($returnValue);
     if (!property_exists($obj,"result")) {
         die(print("Error, no result property"));
     }
     $invalidCreds = "";
-    // check if login was successful
+    // Check if login was successful
     if ($obj->result == "Success") {
         $_SESSION['loggedIn'] = true;
-        // save the user id to the session 
+        // Save the user id to the session 
         $_SESSION["id"] = $obj->data->id;
-	// save the user's name to the session
+	// Save the user's name to the session
         $_SESSION["name"] = $obj->data->name;
         die(header("Location: " . BOOKMARKS));
     } else {
-        // output result stored in session variable, then goes to LOGIN and is displayed there
+        // Output result stored in session variable, then goes to LOGIN and is displayed there
 		$_SESSION['error'] = 'Username or password is incorrect, please try again.';
 		die(header("Location: " . LOGIN));
     }
