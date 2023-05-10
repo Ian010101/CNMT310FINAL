9b10ec03d0e22f4e98524becd0eb6061fdd9665a
Ian Anhalt <96670734+Ian010101@users.noreply.github.com>
2023-05-09 20:52:38 -0500

    Add files via upload

--- bookmarkcards.php
+++ bookmarkcards.php
@@ -1,12 +1,11 @@
 <?php
-
 // Redirect if accessed directly
 if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
     die(header("Location: " . BOOKMARKS));
 }
-
-// Function to create user's bookmark cards
+// Function to create bookmark cards
 function generateBookmarkCards($bookmarks) {
+	
     // Loop through bookmarks array and generate a card for each bookmark
     foreach ($bookmarks as $bookmark) {
         // Bookmark card 
@@ -25,49 +24,11 @@ function generateBookmarkCards($bookmarks) {
                                             print '</a>';
                                         print '</button>';
                                     print '</div>';
-                                    // Bookmark title - Added on click event to refresh page, remove if not needed
-									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank" onclick="setTimeout(function(){location.reload();}, 100);"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
+                                    // Bookmark title
+									print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
                                     // Bookmark url 
-										print '<a href="' . $bookmark->url . '" class="bookmarkURL" target="_blank" onclick="setTimeout(function(){location.reload();}, 100);"><span>' . $bookmark->url . '</span></a>';
-                                    // New line for badges
-                                    print '<div class="clearfix"></div>';
-                                    // Bookmark visibility indicator
-									if ($bookmark->shared) {
-										print '<span class="badge bg-success shared-badge">Public | ID: ' . $bookmark->bookmark_id . '</span>';
-									} else {
-										print '<span class="badge bg-danger private-badge">Private | ID: ' . $bookmark->bookmark_id . '</span>';
-									}
+                                    print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkURL"  target="_blank"><span>' . $bookmark->url . '</span></a>';
                                 print '</div>';
-                            print '</div>';    
-                        print '</div>';
-                    print '</div>';    
-                print '</div>';
-            print '</div>';
-        print '</div>';
-    }
-}
-
-// Function to create public bookmark cards
-function generatePublicBookmarkCards($bookmarks) {
-    // Loop through bookmarks array and generate a card for each bookmark
-    foreach ($bookmarks as $bookmark) {
-        // Bookmark card 
-        print '<div class="d-flex justify-content-center align-items-center mb-2">';
-            print '<div class="container">';
-                print '<div class="row d-flex justify-content-center">';
-                    print '<div class="col-12 col-md-8 col-lg-10">';
-                        print '<div class="card bg-white">';
-                            print '<div class="card-body p-3">';
-                                print '<div>';
-                                    // Bookmark title - Added on click event to refresh page, remove if not needed
-								print '<a href ="addvisit.php?bookmark_id=' . $bookmark->bookmark_id . '&bookmarkurl=' . $bookmark->url .'" class="bookmarkTitle" id="bookmarkVisit" target="_blank" onclick="setTimeout(function(){location.reload();}, 100);"><h5>' . $bookmark->displayname . ' | <span class="visitCount">' . $bookmark->visits . '</span> Visits</h5></a>';
-                                    // Bookmark url 
-									print '<a href="' . $bookmark->url . '" class="bookmarkURL" target="_blank" onclick="setTimeout(function(){location.reload();}, 100);"><span>' . $bookmark->url . '</span></a>';
-                                    // New line for badges
-                                    print '<div class="clearfix"></div>';
-                                    // Bookmark visibility indicator
-									print '<span class="badge bg-success shared-badge">Public | ID: ' . $bookmark->bookmark_id . '</span>';                        
-							    print '</div>';
                             print '</div>';    
                         print '</div>';
                     print '</div>';    
